df5c3fb3fb73352eaf911f71337da3c154c9fc66
kvc55 <karenvirginiacaro@gmail.com>
2022-12-02 17:34:31 -0300

    download button added

--- page/main_page.py
+++ page/main_page.py
@@ -3,22 +3,40 @@ import pathlib
 import requests
 from os import listdir
 from os.path import isfile, join
-import base64
-
 
 import pandas as pd
 import streamlit as st
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-)
+from streamlit_extras.dataframe_explorer import dataframe_explorer
+
 
 from logsetup import log_setup
 
 logger = log_setup.logging.getLogger(__name__)
 logger_r = log_setup.logging.getLogger('result')
+
+
+def add_logo(logo_url: str) -> None:
+    """Add a logo (from logo_url) on the top of the sidebar.
+
+    :param logo_url: URL of the logo
+    :type logo_url: str
+    """    
+ 
+    #validators.url(logo_url)
+
+    st.markdown(
+        f"""
+        <style>
+            [data-testid="stSidebar"] {{
+                background-image: url({logo_url});
+                background-repeat: no-repeat;
+                padding-top: 70px;
+                background-position: 15px 40px;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 def send_file(path: str) -> str:
     """Send csv files interact with FastAPI endpoint.
@@ -30,13 +48,14 @@ def send_file(path: str) -> str:
     files = {'file': open(path, 'rb')}
     try:
         res = requests.post(url, files=files)
+        print('ok')
     except requests.RequestException as e:
         logger.error("OOPS!! General Error")
         logger.error(str(e))
     finally:
         logger_r.info("Always executed complete")
         
-def receive_csv_info(file_name: str)  -> str:
+def receive_csv_info(file_name: str) -> str:
     """Get dataset info from FastAPI endpoint.
 
     Returns:
@@ -45,13 +64,21 @@ def receive_csv_info(file_name: str)  -> str:
     url = f'http://localhost:8000/data/{file_name}'
     try:
         resp=requests.get(url)
+        print(resp.text)
+        return resp.text
     except requests.RequestException as e:
         logger.error("OOPS!! General Error")
         logger.error(str(e))
     finally:
         logger_r.info("Always executed complete")
         
-def save_file():
+def save_file() -> str:
+    """Saves the uploaded .csv file locally.
+
+    :return: Path to data folder
+    :rtype: str
+    """
+
     data = uploaded_file.getvalue().decode('utf-8')
     parent_path = pathlib.Path(__file__).parent.parent.resolve()           
     save_path = os.path.join(parent_path, "data")
@@ -62,165 +89,92 @@ def save_file():
         
     #Send file to server
     send_file(complete_name)
-    #logger_r.info("Upload data and send csv file completed")
+    print(complete_name)
+    logger_r.info("Upload data and send csv file completed")
+
     return save_path
 
 
-def select_file(data_path):
-    onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
-    option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
+def select_file(data_path: str) -> str:
+    """Retrieves the path to specific file.
+
+    :param data_path: Path to data folder
+    :type data_path: str
+    :return: Path to specific file
+    :rtype: str
+    """
+
+    file_list = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+    option = st.sidebar.selectbox('**Pick a dataset**', file_list)
     file_location = os.path.join(data_path, option)
 
     return file_location
 
 
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Adds a UI on top of a dataframe to let viewers filter columns
-    Args:
-        df (pd.DataFrame): Original dataframe
-    Returns:
-        pd.DataFrame: Filtered dataframe
-    """
-    
-    st.write(file_location)
+    """Adds a UI on top of a dataframe to let viewers filter columns.
 
-    modify = st.sidebar.checkbox("Add filters")
+    :param df: Original dataframe
+    :type df: pd.DataFrame
+    :return: Filtered dataframe
+    :rtype: pd.DataFrame
+    """
+
+    modify = st.sidebar.checkbox("**Add filters**")
 
     if not modify:
         return df
 
-    df = df.copy()
+    filter_container = st.sidebar.container()
 
-    # Try to convert datetimes into a standard format (datetime)
-    for col in df.columns:
-        if is_object_dtype(df[col]):
-            try:
-                df[col] = pd.to_datetime(df[col])
-            except Exception:
-                pass
+    with filter_container:
+        filtered_df = dataframe_explorer(df)
 
-        if is_datetime64_any_dtype(df[col]):
-            df[col] = df[col].dt.tz_localize(None)
-
-    modification_container = st.sidebar.container()
-
-    with modification_container:
-        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-        
-        for column in to_filter_columns:
-            left, right = st.columns((1, 20)) # needs review
-            left.write("↳")
-            # Treat columns with < 10 unique values as categorical ---> review cities
-            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                user_cat_input = right.multiselect(
-                    f"Values for {column}",
-                    df[column].unique(),
-                    default=list(df[column].unique()),
-                )
-                df = df[df[column].isin(user_cat_input)]
-            elif is_numeric_dtype(df[column]):
-                _min = float(df[column].min())
-                _max = float(df[column].max())
-                step = (_max - _min) / 100 # ------> review
-                user_num_input = right.slider(
-                    f"Values for {column}",
-                    _min,
-                    _max,
-                    (_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
-            elif is_datetime64_any_dtype(df[column]):
-                user_date_input = right.date_input(
-                    f"Values for {column}",
-                    value=(
-                        df[column].min(),
-                        df[column].max(),
-                    ),
-                )
-                if len(user_date_input) == 2:
-                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                    start_date, end_date = user_date_input
-                    df = df.loc[df[column].between(start_date, end_date)]
-            else:
-                user_text_input = right.text_input(
-                    f"Substring in {column}",
-                )
-                if user_text_input:
-                    df = df[df[column].str.contains(user_text_input)]
-        
-
-
-        want_drop_nulls = st.checkbox("Drop null values")
+        want_drop_nulls = st.checkbox("**Drop null values**")
 
         if want_drop_nulls:
             to_drop_nulls = st.multiselect("Select columns", df.columns)
 
             drop_button = st.button('Apply')
             if drop_button:
-                df.dropna(subset=to_drop_nulls, inplace=True)
+                filtered_df.dropna(subset=to_drop_nulls, inplace=True)
 
-    return df
+    return filtered_df
 
-
-
-def drop_nulls(df: pd.DataFrame) -> pd.DataFrame:
-    want_drop_nulls = st.sidebar.checkbox("Drop null values")
-    
-    if want_drop_nulls:
-        to_drop_nulls = st.sidebar.multiselect("Select columns", df.columns)
-
-        drop_button = st.sidebar.button('Apply')
-        if drop_button:
-            df.dropna(subset=to_drop_nulls, inplace=True)
-
-    return df
             
-#@st.cache(hash_funcs={st.delta_generator.DeltaGenerator: hash})
 @st.cache
-def convert_df(df):
+def convert_df(df: pd.DataFrame) -> bytes:
+    """Converts dataframe to .csv file.
+
+    :param df: Filtered dataframe
+    :type df: pd.DataFrame
+    :return: .csv file
+    :rtype: bytes
+    """
+
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv(index=False).encode('utf-8')
 
 def save_filtered_df(df: pd.DataFrame) -> None:
+    """Donwloads .csv file
+
+    :param df: Filtered dataframe
+    :type df: pd.DataFrame
+    """
+
     save_filtered_df = st.sidebar.checkbox("Save results")
     
     if save_filtered_df:
         csv = convert_df(df)
         st.sidebar.download_button(
-            label="Download data as CSV",
+            label="Download",
             data=csv,
             file_name='large_df.csv',
             mime='text/csv',
         )
 
 
-
-
-def download_link(object_to_download, download_filename, download_link_text):
-
-    if isinstance(object_to_download,pd.DataFrame):
-        object_to_download = object_to_download.to_csv(index=True)
-
-    # some strings <-> bytes conversions necessary here
-    try:
-        # some strings <-> bytes conversions necessary here
-        b64 = base64.b64encode(object_to_download.encode()).decode()
-
-    except AttributeError as e:
-        b64 = base64.b64encode(object_to_download).decode()
-
-    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
-
-def save_df(df):
-    if st.button('Download Dataframe as CSV'):
-        tmp_download_link = download_link(df, 'YOUR_DF.csv', 'Click here to download your data!')
-        st.markdown(tmp_download_link, unsafe_allow_html=True)
-
-
-
-
+# Main page structure
 
 st.title("TESSERACT")
 
@@ -229,27 +183,37 @@ st.markdown(
     """
 )
 
+add_logo("https://res.cloudinary.com/dqgvfwvst/image/upload/v1669991725/tesseract_kwvpou.jpg")
 
 st.subheader('Choose a file')
+
 uploaded_file = st.file_uploader((''))
+# All tasks to be done when the user uploads a .csv file
 if uploaded_file is not None:
   data_path = save_file()
   file_location = select_file(data_path)
   df = pd.read_csv(file_location)
-  filtered_dataframe = st.dataframe(filter_dataframe(df))
-    
-  #Button display dataset info
-  if st.button('Resume dataset'):
+
+  # Operations that the user can apply 
+  filtered_df = filter_dataframe(df)
+  st.dataframe(filtered_df, use_container_width=True)
+
+  save_filtered_df(filtered_df)
+
+  # Button display dataset info
+  st.sidebar.markdown(
+    """**Show dataset information**
+    """
+  )
+  if st.sidebar.button('Ask'):
     dataset_info = receive_csv_info(uploaded_file.name)
     st.text(dataset_info)
   else:
-    st.write('Received info complete')
+    st.sidebar.write('Received info complete')
+  
+
+ 
+  
+ 
     
-  #st.write(type(filtered_dataframe))
-  #st.dataframe(drop_nulls(filtered_dataframe))
-
-  #save_filtered_df(filtered_dataframe)
-
-  #convert_df(filtered_dataframe)
-
-#save_df(df)
+  
