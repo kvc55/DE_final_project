e03f7ee80974486825f91298485004aaad324f4b
senocax <emilianodemo@gmail.com>
2022-12-07 09:11:17 -0300

    Add test returns Dataframe

--- tests/test_etl_to_db.py
+++ tests/test_etl_to_db.py
@@ -37,12 +37,18 @@ class TestCalculator(unittest.TestCase):
 
         # Checks not existence of dropped column
         self.assertNotIn(list(result.columns), ['customer_unique_id'])
+        
+        # Checks returns a dataframe 
+         self.assertEqual(type(result)), type(pd.DataFrame()))
 
     def test_etl_geolocation(self) -> None:
         result = etl.etl_geolocation(test_csv_geolocation)
 
         # Checks total columns
         self.assertEqual((len(result.columns)), 5)
+     
+        # Checks returns a dataframe 
+        self.assertEqual(type(result)), type(pd.DataFrame()))
 
     def test_etl_order_items(self) -> None:
         result = etl.etl_order_items(test_csv_items)
@@ -53,6 +59,9 @@ class TestCalculator(unittest.TestCase):
         # Checks not existence of dropped column 
         self.assertNotIn(list(result.columns), ['order_item_id'])
         self.assertEqual(result.shipping_limit_date.dtype, 'datetime64[ns]')
+        
+        # Checks returns a dataframe 
+        self.assertEqual(type(result)), type(pd.DataFrame()))
 
     def test_etl_order_payments(self) -> None:
         result = etl.etl_order_payments(test_csv_payments)
@@ -62,6 +71,9 @@ class TestCalculator(unittest.TestCase):
 
         # Checks not existence of dropped column
         self.assertNotIn(list(result.columns), ['payment_sequential'])
+        
+        # Checks returns a dataframe 
+        self.assertEqual(type(result)), type(pd.DataFrame()))
 
     def test_etl_order_review(self) -> None:
         result = etl.etl_order_review(test_csv_reviews)
@@ -75,6 +87,9 @@ class TestCalculator(unittest.TestCase):
         # Checks type of converted values
         self.assertEqual(result.review_creation_date.dtype, 'datetime64[ns]')
         self.assertEqual(result.review_answer_timestamp.dtype, 'datetime64[ns]')
+        
+        # Checks returns a dataframe 
+        self.assertEqual(type(result)), type(pd.DataFrame()))
 
     def test_etl_orders(self) -> None:
         result = etl.etl_orders(test_csv_orders)
@@ -88,6 +103,9 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(result.order_delivered_carrier_date.dtype, 'datetime64[ns]')
         self.assertEqual(result.order_delivered_customer_date.dtype, 'datetime64[ns]')
         self.assertEqual(result.order_estimated_delivery_date.dtype, 'datetime64[ns]')
+        
+        # Checks returns a dataframe 
+        self.assertEqual(type(result)), type(pd.DataFrame()))
 
     def test_etl_products(self) -> None:
         result = etl.etl_products(test_file_products)
@@ -103,12 +121,18 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(result.product_length_cm.dtype, 'int64')
         self.assertEqual(result.product_height_cm.dtype, 'int64')
         self.assertEqual(result.product_width_cm.dtype, 'int64')
+        
+        # Checks returns a dataframe 
+        self.assertEqual(type(result)), type(pd.DataFrame()))
 
     def test_etl_sellers(self) -> None:
         result = etl.etl_sellers(test_file_sellers)
 
         # Checks total columns 
         self.assertEqual((len(result.columns)), 4)
+        
+        # Checks returns a dataframe 
+        self.assertEqual(type(result)), type(pd.DataFrame()))
 
 
 if __name__ == '__main__':
