2fb0a60f4fa6e54754599544844a822932450095
senocax <emilianodemo@gmail.com>
2022-12-07 00:14:10 -0300

    Add setup test data

--- tests/test_etl_to_db.py
+++ tests/test_etl_to_db.py
@@ -9,10 +9,28 @@ import src.etl_to_db as etl
 class TestCalculator(unittest.TestCase):
 
     def setUp(self) -> None:
-        pass
+    """ Load data setUp """
+    TEST_INPUT_DIR = 'data_input/'
+    test_file_customer =  'olist_customers_dataset.csv'
+    test_file_geolocation =  'olist_geolocation_dataset.csv'
+    test_file_items =  'olist_order_items_dataset.csv'
+    test_file_payments =  'olist_order_payments_dataset.csv'
+    test_file_reviews =  'olist_order_reviews_dataset.csv'
+    test_file_orders=  'olist_orders_dataset.csv'
+    test_file_products=  'olist_products_dataset.csv'
+    test_file_sellers=  'olist_sellers_dataset.csv'
+    
+    self.test_csv_customer = TEST_INPUT_DIR + test_file_customer
+    self.test_csv_geolocation = TEST_INPUT_DIR + test_file_geolocation
+    self.test_csv_items = TEST_INPUT_DIR + test_file_items
+    self.test_csv_payments = TEST_INPUT_DIR + test_file_payments
+    self.test_csv_reviews = TEST_INPUT_DIR + test_file_reviews
+    self.test_csv_orders = TEST_INPUT_DIR + test_file_orders
+    self.test_csv_products = TEST_INPUT_DIR + test_file_products
+    self.test_csv_sellers = TEST_INPUT_DIR + test_file_sellers
 
     def test_etl_customers(self) -> None:
-        result = etl.etl_customers()
+        result = etl.etl_customers(test_csv_customer)
 
         # Checks total columns 
         self.assertEqual((len(result.columns)), 4)
@@ -21,13 +39,13 @@ class TestCalculator(unittest.TestCase):
         self.assertNotIn(list(result.columns), ['customer_unique_id'])
 
     def test_etl_geolocation(self) -> None:
-        result = etl.etl_geolocation()
+        result = etl.etl_geolocation(test_csv_geolocation)
 
         # Checks total columns
         self.assertEqual((len(result.columns)), 5)
 
     def test_etl_order_items(self) -> None:
-        result = etl.etl_order_items()
+        result = etl.etl_order_items(test_csv_items)
 
         # Checks total columns
         self.assertEqual((len(result.columns)), 6)
@@ -37,7 +55,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(result.shipping_limit_date.dtype, 'datetime64[ns]')
 
     def test_etl_order_payments(self) -> None:
-        result = etl.etl_order_payments()
+        result = etl.etl_order_payments(test_csv_payments)
 
         # Checks total columns
         self.assertEqual((len(result.columns)), 4)
@@ -46,7 +64,7 @@ class TestCalculator(unittest.TestCase):
         self.assertNotIn(list(result.columns), ['payment_sequential'])
 
     def test_etl_order_review(self) -> None:
-        result = etl.etl_order_review()
+        result = etl.etl_order_review(test_csv_reviews)
 
         # Checks total columns 
         self.assertEqual((len(result.columns)), 6)
@@ -59,7 +77,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(result.review_answer_timestamp.dtype, 'datetime64[ns]')
 
     def test_etl_orders(self) -> None:
-        result = etl.etl_orders()
+        result = etl.etl_orders(test_csv_orders)
 
         # Checks total colummns
         self.assertEqual((len(result.columns)), 8)
@@ -72,7 +90,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(result.order_estimated_delivery_date.dtype, 'datetime64[ns]')
 
     def test_etl_products(self) -> None:
-        result = etl.etl_products()
+        result = etl.etl_products(test_file_products)
 
         # Checks total columns
         self.assertEqual((len(result.columns)), 9)
@@ -87,7 +105,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(result.product_width_cm.dtype, 'int64')
 
     def test_etl_sellers(self) -> None:
-        result = etl.etl_sellers()
+        result = etl.etl_sellers(test_file_sellers)
 
         # Checks total columns 
         self.assertEqual((len(result.columns)), 4)
