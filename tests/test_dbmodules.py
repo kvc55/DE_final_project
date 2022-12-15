06f884c714d802d225b53e516c06cb3bf6ae82bf
senocax <emilianodemo@gmail.com>
2022-12-15 12:15:22 -0300

    Add optimizations test db

--- tests/test_dbmodules.py
+++ tests/test_dbmodules.py
@@ -26,43 +26,47 @@ class DBmodules(unittest.TestCase):
         host = os.getenv('HOST')
         dbname = os.getenv('DBNAME')
 
-        db = Database(user,password,host,dbname)
+        # Set test connection
+        db = Database(user, password, host, dbname)
         self.test_db= db
 
+        # Set test dataset
+        self.dataset = pd.DataFrame({
+            'customer_unique_id': [1, 2, 3],
+            'customer_city': ['rio de janeiro', 'sao paulo', 'brasilia']
+        })
+ 
     def test_bulkInsert(self):
         """Test insert table.
         """
-        dataset = pd.DataFrame({
-            'id': [1, 2, 3],
-            'customer': ['Juan', 'Pedro', 'Maria']
-        })
-        assert self.test_db.bulkInsert(dataset, "id")        
+        assert self.test_db.bulkInsert(self.dataset, "customer_unique_id")        
 
     def test_fetchByTable(self):
         """Test fetch table.
         """
-        assert self.test_db.fetchByTable("id")
+        assert self.test_db.fetchByTable("customer_unique_id")
 
     def test_fetchByQuery(self):
-        """Test fetch table.
+        """Test fetch query.
         """
-        assert self.test_db.fetchByQuery("SELECT * FROM id")
+        assert self.test_db.fetchByQuery("SELECT * FROM customer_unique_id")
 
     def test_dinorderquery(self):
-        """Test fetch table.
+        """Test order query.
         """
-        assert self.test_db.dinorderquery("id", id='ASC')
+        assert self.test_db.dinorderquery("customer_unique_id", id= 'DESC')
 
     def test_dinfilterqueryand(self):
-        """Test fetch table.
+        """Test filter AND query.
         """
-        assert self.test_db.dinfilterqueryand("id", id='ASC')
+        assert self.test_db.dinfilterqueryand("customer_unique_id", id= '>2')
 
     def test_dinfilterqueryor(self):
-        """Test fetch table.
+        """Test filter query.
         """
-        assert self.test_db.dinfilterqueryor("id", id='ASC')
+        assert self.test_db.dinfilterqueryor("customer_city", city= 'brasilia')
 
 
 if __name__ == '__main__':
     unittest.main()
+
